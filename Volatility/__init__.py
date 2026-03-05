f6cb319115d5c6581ac30a19546f8ad9ea61cba1
Umer Farooq <umerfarooq230@gmail.com>
2026-03-05 19:07:16 +0500

    Updated modules

--- Volatility/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "0.1.1"
-from .realized import RollingSTD, EWMA
-from .range_based import Parkinson, GarmanKlass
-from .vol_of_vol import VolOfVol
-from .asymmetric import DownSideVolatility
-from .evaluation import evaluate, rmse
---
+++ sixeyes/__init__.py
@@ -0,0 +1,2 @@
+from . import Volatility
+from . import Regime
